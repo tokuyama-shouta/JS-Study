69e43f5715e49a9164fd689d2b12a67a4823d5ee
tokuyama shouta <toku1031ts@gmail.com>
2021-01-01 22:23:54 +0900

    コンストラクター関数

---
+++ index.js
@@ -0,0 +1,10 @@
+//コンストラクター関数
+
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+}
+
+const bob = new Person('Bob', 18);
+const tom = new Person('Tom', 33);
+const sun = new Person('Sun', 20);
