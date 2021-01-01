a9b3c9ab3cbdc5a5146f18de4ce74c52875d7bff
tokuyama shouta <toku1031ts@gmail.com>
2021-01-01 22:48:09 +0900

    new演算子

--- index.js
+++ index.js
@@ -20,3 +20,28 @@ bob.hello();
 
 //prototype
 //オブジェクトに存在する特別なプロパティー
+
+
+//new 演算子
+
+function F(a, b) {
+  this.a = a;
+  this.b = b;
+  return {};
+}
+
+F.prototype.c = function() {}
+
+function newOpe(c, ...args) {
+  const _this = Object.create(C.prototype);
+  const result = C.apply(_this, args)
+  console.log(typeof result);
+  if( typeof result === "object" && result !== null){
+    return result;
+  } 
+
+  return _this;
+}
+
+const instance = newOpe(F, 1, 2);
+console.log(instance)
